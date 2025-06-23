4488e6800bb7c63bc6a1ba1b1a6dbaedfb1c45d7
Taegyun Kim <kimtg2004@gmail.com>
2025-06-23 16:53:02 +0900

    printf를 이용한 출력 형식

--- SimpleProject/Hello.c
+++ SimpleProject/Hello.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+/*
+* 제목: Hello world 출력하기
+* 기능: 문자열의 출력
+* 수정날짜: 2025. 06. 23.
+* 작성자: 김태균
+*/
 
-int main(void)
+
+
+#include <stdio.h> //헤더파일 선언
+
+int main1(void) // main 함수의 시작
 {
-	printf("hello world! \n");
-	return 0;
-}
+	/*
+	* 이 함수 내에서는 하나의 문자열을 출력한다.
+	* 문자열은 모니터로 출력된다.
+	*/
+	printf("hello world! \n"); // 문자열의 출력
+	return 0;  // 0의 반환
+} //함수의 끝
---
+++ SimpleProject/Return_Type.c
@@ -0,0 +1,7 @@
+int main(void) 
+{
+	printf("My age: %d \n", 20);
+	printf("%d is my point\n", 100);
+	printf("Good \nMorning \nEverybody\n");
+	return 0;
+}
